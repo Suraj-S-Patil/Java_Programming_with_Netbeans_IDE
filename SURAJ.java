67690685f912f7ebf4e53d6b062123e4fe5cfa1d
Suraj Patil <52925856+Suraj-S-Patil@users.noreply.github.com>
2020-01-14 19:41:13 +0530

    Add files via upload

---
+++ SURAJ.java
@@ -0,0 +1,19 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package suraj;
+
+/**
+ *
+ * @author TEACHER
+ */
+public class SURAJ {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+}
